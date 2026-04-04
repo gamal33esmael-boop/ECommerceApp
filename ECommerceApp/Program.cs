eb8ea17d5f80b5b1e0661c3c96b89b307f844335
Gamal Esmael <gamal33esmael@gmail.com>
2026-04-04 13:48:35 +0200

    2

--- ECommerceApp/Program.cs
+++ ECommerceApp/Program.cs
@@ -52,7 +52,7 @@ namespace ECommerceApp
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
-            app.UseRouting(); //Gemy
+            app.UseRouting(); //G
 
             // 4. ???? ?? ??????? ??? ??????
             app.UseAuthentication();
